b91459e8e5d2be36a99e7eaba252d6667ab72987
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-08-05 15:09:37 +0300

    add HW1

---
+++ src/components/Article.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import CommentList from './CommentList'
+
+export default class Article extends Component {
+    state = {
+        isOpen: false
+    }
+
+/*
+    constructor() {
+        super()
+        this.state = {
+            isOpen: false
+        }
+    }
+*/
+
+    render() {
+        const { article } = this.props
+        return (
+            <div>
+                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    toggleOpen = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    getBody() {
+        if (!this.state.isOpen) return null
+        return (
+            <section>
+                {this.props.article.text}
+                <CommentList comments = {this.props.article.comments} />
+            </section>
+        )
+    }
+}
