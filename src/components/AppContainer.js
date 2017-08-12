d4fab10403b9d2acf2d264c09357676d43445e5a
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-08-12 23:28:13 +0300

    add css module

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,14 @@
 import React, { PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import Select from './UserForm'
+import Filter from './Filter'
 
 function AppContainer (props) {
     return (
        <div>
             <UserForm/>
+            <Filter articles = {props.articles}/>
             <ArticleList articles = {props.articles} />
 
        </div>
